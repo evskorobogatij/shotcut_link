70e39ef9224d1001f22ea59a7b4da9e48b723ab0
Скоробогатый Евгений <evskorobogatij@gmail.com>
2022-05-26 09:23:20 +0300

    при регистрации пользователя отдаем  jwt токен

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,20 @@ import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('/register')
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.usersService.create(createUserDto);
+    const res = await this.authService.login({ id: user.id, name: user.name });
+    return res;
   }
 
   //for test
